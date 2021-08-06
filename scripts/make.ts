8304f9bb3705f7c6c84c2f186f3d0a90868458bd
Kok Kek <kokkekpek@gmail.com>
2021-08-06 16:09:33 +0300

    Update linker version to `0.13.2`

--- scripts/make.ts
+++ scripts/make.ts
@@ -11,9 +11,12 @@ make({
         'src/contracts/storages/crystal/CrystalStorage',
 
         // Tests
-        'tests/_src/contracts/CrystalStorageOwner'
+        'tests/_src/contracts/CrystalStorageOwner',
+
+        'x/contracts/A',
+        'x/contracts/B'
     ],
     compiler: '0.47.0',
-    linker: '0.11.90',
+    linker: '0.13.2',
     stdlib: '0.47.0'
 }).then().catch((e: any) => console.log(e))
